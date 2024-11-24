8c1defcf6aff4726b7d52feb57f394626b4f3198
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:11:42 -0300

    feat: Adiciona AviaoModel

---
+++ LP1/src/main/java/com/example/lp1/models/AviaoModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class AviaoModel {
+    private int numeroSerie;
+    private String fabricante;
+    private String modelo;
+
+    public int getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(int numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
