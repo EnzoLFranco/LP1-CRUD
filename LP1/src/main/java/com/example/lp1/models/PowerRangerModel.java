3bb0b474937862c8d0c905e31f63b10f5b7d24d2
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:12:30 -0300

    feat: Adiciona PowerRangerModel

---
+++ LP1/src/main/java/com/example/lp1/models/PowerRangerModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class PowerRangerModel {
+    private String nome;
+    private String corUniforme;
+    private String zord;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCorUniforme() {
+        return corUniforme;
+    }
+
+    public void setCorUniforme(String corUniforme) {
+        this.corUniforme = corUniforme;
+    }
+
+    public String getZord() {
+        return zord;
+    }
+
+    public void setZord(String zord) {
+        this.zord = zord;
+    }
+}
