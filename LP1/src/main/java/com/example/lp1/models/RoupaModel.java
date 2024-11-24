3e95f7480c7a1c7b550902859e421ca4fc7ed677
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:12:38 -0300

    feat: Adiciona RoupaModel

---
+++ LP1/src/main/java/com/example/lp1/models/RoupaModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class RoupaModel {
+    private int IDRoupa;
+    private String tipo;
+    private String tamanho;
+
+    public int getIDRoupa() {
+        return IDRoupa;
+    }
+
+    public void setIDRoupa(int ID) {
+        this.IDRoupa = ID;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tm) {
+        this.tamanho = tm;
+    }
+}
