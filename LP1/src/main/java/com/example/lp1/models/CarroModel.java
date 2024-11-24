9a3ccaff3cb97d6d8d38f67b598c9ba01aaacbd8
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:11:52 -0300

    feat: Adiciona CarroModel

---
+++ LP1/src/main/java/com/example/lp1/models/CarroModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class CarroModel {
+    private String placa;
+    private String marca;
+    private String modelo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String pl) {
+        this.placa = pl;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String mc) {
+        this.marca = mc;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String md) {
+        this.modelo = md;
+    }
+}
