f64bacdb5981f32dd225dcff8127a6339431e461
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:12:46 -0300

    feat: Adiciona VideoGameModel

---
+++ LP1/src/main/java/com/example/lp1/models/VideoGameModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class VideoGameModel {
+    private String nome;
+    private String genero;
+    private int classificacaoEtaria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getClassificacaoEtaria() {
+        return classificacaoEtaria;
+    }
+
+    public void setClassificacaoEtaria(int classificacaoEtaria) {
+        this.classificacaoEtaria = classificacaoEtaria;
+    }
+}
