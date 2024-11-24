e91a7111dc4814a49ab8a3dee296d6273a9b7101
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 15:58:38 -0300

    feat: Adiciona o AnimalModel

---
+++ LP1/src/main/java/com/example/lp1/models/AnimalModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class AnimalModel {
+    private int IDAnimal;
+    private String nome;
+    private String especie;
+
+    public int getIDAnimal() {
+        return IDAnimal;
+    }
+
+    public void setIDAnimal(int IDAnimal) {
+        this.IDAnimal = IDAnimal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+}
