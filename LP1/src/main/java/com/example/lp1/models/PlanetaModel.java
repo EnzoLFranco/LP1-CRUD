9dc72715839d3488a856fd8adcbd1192a8bb9d3a
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:12:11 -0300

    feat: Adiciona PlanetaModel

---
+++ LP1/src/main/java/com/example/lp1/models/PlanetaModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class PlanetaModel {
+    private String nome;
+    private float raio;
+    private String massa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getRaio() {
+        return raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+
+    public String getMassa() {
+        return massa;
+    }
+
+    public void setMassa(String massa) {
+        this.massa = massa;
+    }
+}
