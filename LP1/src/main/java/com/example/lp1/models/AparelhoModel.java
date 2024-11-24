3da3de3f3c740f483edeaa98fe0bd785b2f0cc34
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:01:13 -0300

    feat: Adiciona AparelhoModel

---
+++ LP1/src/main/java/com/example/lp1/models/AparelhoModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class AparelhoModel {
+    private int numeroSerie;
+    private String marca;
+    private String tipo;
+
+    public int getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(int numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
