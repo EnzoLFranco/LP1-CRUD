5783f870c7fd5eb1f5155f8062b6a4b9044cb997
EnzoLFranco <fenzolemos@gmail.com>
2024-11-24 16:12:19 -0300

    feat: Adiciona PokemonModel

---
+++ LP1/src/main/java/com/example/lp1/models/PokemonModel.java
@@ -0,0 +1,31 @@
+package com.example.lp1.models;
+
+public class PokemonModel {
+    private int numeroPokedex;
+    private String nome;
+    private String tipos;
+
+    public int getNumeroPokedex() {
+        return numeroPokedex;
+    }
+
+    public void setNumeroPokedex(int numeroPokedex) {
+        this.numeroPokedex = numeroPokedex;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(String tipos) {
+        this.tipos = tipos;
+    }
+}
