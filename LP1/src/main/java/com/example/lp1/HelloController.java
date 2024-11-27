be64f716924c80bdf1e388a6a23b383a32bc5357
EnzoLFranco <fenzolemos@gmail.com>
2024-11-27 17:20:39 -0300

    feat: Adiciona o método que vai pro Cadastro

--- LP1/src/main/java/com/example/lp1/HelloController.java
+++ LP1/src/main/java/com/example/lp1/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.lp1;
 
+import com.example.lp1.helpers.DatabaseConnection;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class HelloController {
     private Stage stage;
@@ -14,7 +21,7 @@ public class HelloController {
     private Parent root;
     private String cssTelas = getClass().getResource("styleTelas.css").toExternalForm();
     private String css = getClass().getResource("style.css").toExternalForm();
-    
+
     public void trocaAnimais(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneAnimal.fxml"));
         Scene scene = new Scene(root);
@@ -23,6 +30,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaAviões(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneAviao.fxml"));
         Scene scene = new Scene(root);
@@ -31,6 +39,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaCarros(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneCarro.fxml"));
         Scene scene = new Scene(root);
@@ -39,6 +48,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaInstrumentos(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneInstrumento.fxml"));
         Scene scene = new Scene(root);
@@ -47,6 +57,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaRoupas(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneRoupa.fxml"));
         Scene scene = new Scene(root);
@@ -55,6 +66,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaPokemons(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ScenePokemon.fxml"));
         Scene scene = new Scene(root);
@@ -63,6 +75,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaVideoGames(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SceneVG.fxml"));
         Scene scene = new Scene(root);
@@ -71,6 +84,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaPowerRangers(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ScenePR.fxml"));
         Scene scene = new Scene(root);
@@ -79,6 +93,7 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaPlanetas(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ScenePlaneta.fxml"));
         Scene scene = new Scene(root);
@@ -87,23 +102,36 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void trocaAparelhos(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("SceneAparelho.fxml")); // Corrigido o carregamento do FXML
+        Parent root = FXMLLoader.load(getClass().getResource("SceneAparelho.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(cssTelas);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
+
+    public void trocaCadastro(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Cadastro.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void voltaMenu(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml")); // Corrigido o carregamento do FXML
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(css);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
-    public void Exit(ActionEvent e){
+
+    public void Exit(ActionEvent e) {
         System.exit(0);
     }
+
+
 }
