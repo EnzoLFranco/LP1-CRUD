72dea360f34a21726673f581c9e72a60f8c97350
EnzoLFranco <fenzolemos@gmail.com>
2024-11-28 17:16:45 -0300

    feat: Adiciona a leitura do banco no controller do Carro

--- LP1/src/main/java/com/example/lp1/CarroController.java
+++ LP1/src/main/java/com/example/lp1/CarroController.java
@@ -1,9 +1,20 @@
 package com.example.lp1;
 
+import com.example.lp1.helpers.DatabaseConnection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.example.lp1.models.CarroModel;
 
 public class CarroController extends HelloController {
     private String placa;
@@ -16,6 +27,14 @@ public class CarroController extends HelloController {
     private TextField modeloInput;
     @FXML
     private TextField marcaInput;
+    @FXML
+    private TableView<CarroModel> tableView;
+    @FXML
+    private TableColumn<CarroModel, String> placaColumn;
+    @FXML
+    private TableColumn<CarroModel, String> marcaColumn;
+    @FXML
+    private TableColumn<CarroModel, String> modeloColumn;
 
     public String getPlaca() {
         return placa;
@@ -34,6 +53,46 @@ public class CarroController extends HelloController {
     }
     public void setModelo(String md) {
         this.modelo = md;
+    }
+
+    public void initialize() {
+        placaColumn.setCellValueFactory(new PropertyValueFactory<>("placa"));
+        marcaColumn.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        modeloColumn.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+
+        carregarTabela();
+    }
+
+    private void carregarTabela() {
+        List<CarroModel> carros = getCarrosDoBanco();
+
+        tableView.getItems().clear();
+        tableView.getItems().addAll(carros);
+    }
+
+    private List<CarroModel> getCarrosDoBanco() {
+        List<CarroModel> carros = new ArrayList<>();
+        String sql = "SELECT * FROM Carro";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             ResultSet rs = conn.createStatement().executeQuery(sql)) {
+
+            while (rs.next()) {
+                String placa = rs.getString("placa");
+                String marca = rs.getString("marca");
+                String modelo = rs.getString("modelo");
+
+                System.out.println("Carro encontrado: ID = " + placa + ", Marca = " + marca + ", Modelo = " + modelo);
+
+                CarroModel carro = new CarroModel(placa, marca, modelo);
+                carros.add(carro);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return carros;
     }
 
     public void ligar(ActionEvent event) {
